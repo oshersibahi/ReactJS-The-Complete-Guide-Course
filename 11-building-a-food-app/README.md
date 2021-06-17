083dacd3ccfe0363b2a37ad4d354f91d57bcc334
Osher Sibahi <oshersibahi@gmail.com>
2021-06-17 16:24:45 +0300

    Section 11: Practice Project: Building a Food Order App

---
+++ 11-building-a-food-app/README.md
@@ -0,0 +1,4 @@
+Section 11: Practice Project: Building a Food Order App.
+
+All of the project is fully created and directed by Maximilian.
+No original code.
---
+++ 11-building-a-food-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import CartContext from '../../store/cart-context';
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false); 
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+  //currNumber is the value passed betweeen iterations, the initial value is the first currNumber value.
+  //Reduce function on arrays - let you present the array by a single value.
+  const numberOfCartItems = items.reduce((currNumber, item)=>{
+    return currNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
